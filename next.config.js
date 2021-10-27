da574e1280525bd4d73004120dde5e6fce556c4a
Yonatan <yonatane504@gmail.com>
2021-10-27 13:22:47 +0300

    changed next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,21 @@
+const withCSS = require("@zeit/next-css");
+
+module.exports = withCSS({
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
+      use: {
+        loader: "url-loader",
+        options: {
+          limit: 100000,
+          name: "[name].[ext]",
+        },
+      },
+    });
+    return config;
+  },
+});
+
 module.exports = {
   reactStrictMode: true,
-}
+};
